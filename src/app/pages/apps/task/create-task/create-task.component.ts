ce2b8a5653b5200f994f3f524ad3a84f7f409b9b
nzansv <63594033+nzansv@users.noreply.github.com>
2022-02-20 15:41:04 +0600

    search dropdown

--- src/app/pages/apps/task/create-task/create-task.component.ts
+++ src/app/pages/apps/task/create-task/create-task.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {TaskService} from '../../../../core/service/task.service';
 import {UserService} from '../../../../core/service/user.service';
 import {User} from '../../../../core/model/user';
@@ -7,7 +7,7 @@ import {DatePipe} from '@angular/common';
 import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import {ReplaySubject, Subject} from 'rxjs';
 import {MatSelect} from '@angular/material/select';
-import {take, takeUntil} from 'rxjs/operators';
+import {debounceTime, delay, filter, map, take, takeUntil, tap} from 'rxjs/operators';
 
 interface Food {
   value: string;
@@ -22,11 +22,21 @@ interface Food {
 export class CreateTaskComponent implements OnInit, OnDestroy {
   currentUser: any;
   users: User[];
-  public userCtrl: FormControl = new FormControl();
-  public userFilterCtrl: FormControl = new FormControl();
-  public filteredUsers: ReplaySubject<any> = new ReplaySubject(1);
-  @ViewChild('singleSelect', { static: true }) singleSelect: MatSelect;
-  protected _onDestroy = new Subject();
+  /** control for the selected bank for server side filtering */
+  public usersServerSideCtrl: FormControl = new FormControl();
+
+  /** control for filter for server side. */
+  public usersServerSideFilteringCtrl: FormControl = new FormControl();
+
+  /** indicate search operation is in progress */
+  public searching = false;
+
+  /** list of banks filtered after simulating server side search */
+  public  filteredServerSideUsers: ReplaySubject<User[]> = new ReplaySubject<User[]>(1);
+
+  /** Subject that emits when the component has been destroyed. */
+  protected _onDestroy = new Subject<void>();
+
 
 
   task: Task = {
@@ -50,52 +60,43 @@ export class CreateTaskComponent implements OnInit, OnDestroy {
     this._onDestroy.next();
     this._onDestroy.complete();
   }
-  protected setInitialValue() {
-    this.filteredUsers
-        .pipe(take(1), takeUntil(this._onDestroy))
-        .subscribe(() => {
-          this.singleSelect.compareWith = (a: User, b: User) => a && b && a.id === b.id;
-        });
-  }
 
   /**
    * Write code on Method
    *
    * method logical code
    */
-  protected filterBanks() {
-    if (!this.users) {
-      return;
-    }
 
-    let search = this.userFilterCtrl.value;
-    if (!search) {
-      this.filteredUsers.next(this.users.slice());
-      return;
-    } else {
-      search = search.toLowerCase();
-    }
-
-    this.filteredUsers.next(
-        this.users.filter(user => user.firstName.toLowerCase().indexOf(search) > -1)
-    );
-  }
   createForm() {
-    this.taskForm.controls.employeeId.setValue(this.userCtrl.value?.userId);
+    this.taskForm.controls.employeeId.setValue(this.usersServerSideCtrl.value?.userId);
     this.taskService.createTask(this.taskForm.getRawValue()).subscribe(res => {
     });
   }
   getAllUsers(){
     this.userService.getUsers().subscribe(res => {
       this.users = res;
-      this.userCtrl.setValue(this.users[0]);
-      this.filteredUsers.next(this.users.slice());
-
-      this.userFilterCtrl.valueChanges
-          .pipe(takeUntil(this._onDestroy))
-          .subscribe(() => {
-            this.filterBanks();
-          });
+      this.usersServerSideFilteringCtrl.valueChanges
+          .pipe(
+              filter(search => !!search),
+              tap(() => this.searching = true),
+              takeUntil(this._onDestroy),
+              debounceTime(200),
+              map(search => {
+                if (!this.users) {
+                  return [];
+                }
+                return this.users.filter(user => user.firstName.indexOf(search) > -1);
+              }),
+              delay(500),
+              takeUntil(this._onDestroy)
+          )
+          .subscribe(filteredUsers => {
+                this.searching = false;
+                this.filteredServerSideUsers.next(filteredUsers);
+              },
+              error => {
+                this.searching = false;
+              });
     });
   }
   createTaskForm(): FormGroup {
@@ -103,7 +104,7 @@ export class CreateTaskComponent implements OnInit, OnDestroy {
     this.task.doctorId = this.currentUser.id;
     return this.fb.group({
       createdAt:  Date.now(),
-      employeeId: [this.userCtrl.value?.userId],
+      employeeId: [this.usersServerSideCtrl.value?.userId],
       doctorId: this.currentUser.id,
       complaints: [this.task.complaints],
       recommendations: [this.task.recommendations]
